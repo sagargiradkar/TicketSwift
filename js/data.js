63d0d1cd15bb51abc5e2d47ea5d6f77ed7a70685
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-10-15 17:05:08 +0530

    Movies website

---
+++ js/data.js
@@ -0,0 +1,21 @@
+let movies = [
+
+    {
+        image: 'img/banner/banner3.webp'
+    },
+    {
+        image: 'img/banner/banner4.webp'
+    },
+    {
+        image: 'img/banner/banner6.webp'
+    },
+    {
+        image: 'img/banner/banner7.webp'
+    },
+    {
+        image: 'img/banner/banner8.webp'
+    },
+    {
+        image: 'img/banner/banner9.webp'
+    }
+]
