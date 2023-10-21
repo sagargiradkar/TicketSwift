39a56fdcd7f5f07392e1491f503c5364b6e89550
pavanpatil28 <140961106+pavanpatil28@users.noreply.github.com>
2023-10-21 22:02:20 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,18 @@
+let movies = [
+
+    {
+        image: 'img/banner/banner3.webp'
+    },
+    {
+        image: 'img/banner/banner6.webp'
+    },
+    {
+        image: 'img/banner/banner7.webp'
+    },
+    {
+        image: 'img/banner/banner8.webp'
+    },
+    {
+        image: 'img/banner/banner9.webp'
+    }
+]
---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault();  // Prevent the default form submission behavior
+
+    // Handle the form submission (you can add your own logic here)
+
+    // Redirect to the home page (replace "home.html" with your actual home page URL)
+    window.location.href = "Profile.html";
+});
